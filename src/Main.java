6165ba081974f7ca2eb58bd045628e6efeefab25
Pavel Petronjuk <hardboatguy@gmail.com>
2024-09-30 17:07:26 +0300

    modify Main.java, add F>C functional develop branch;

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Converter_tmpr, ver_0.1");
- //       double fahrenheit = 100;
+        double fahrenheit = 100;
 //        double celsius = 37.78;
 
- //       System.out.println(fahrenheit + " F = " + ((fahrenheit - 32) * 5 / 9) + " C");
+        System.out.println(fahrenheit + " F = " + ((fahrenheit - 32) * 5 / 9) + " C");
 //        System.out.println(celsius + " C = " + ((celsius * 9 / 5) + 32) + " F");
 
     }
